cbe4c755faf51c0d9a261f931efae26f47be2c0b
Paradox369 <george.ketcha@gmail.com>
2024-01-26 11:21:54 +0100

    preping for deployment

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const taskRoutes = require("./routes/taskRoute");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: ["http://localhost:3000/", ""],
+    })
+);
+
+// routes
+app.use("/api/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+    res.send("hello George");
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`db connected and server running on port: ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
